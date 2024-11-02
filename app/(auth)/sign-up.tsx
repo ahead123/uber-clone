6aac87e35b8b02496cb8688c68da47ae988109ab
Arthur Head <arthurhead@Arthurs-MacBook-Pro.local>
2024-11-02 16:26:27 -0700

    added neon db, and api route for sign up

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -7,6 +7,7 @@ import CustomButton from '@/components/CustomButton';
 import OAuth from '@/components/OAuth';
 import { useSignUp } from '@clerk/clerk-expo';
 import ReactNativeModal from 'react-native-modal';
+import { fetchAPI } from '@/lib/fetch';
 
 const SignUp = () => {
   const { isLoaded, signUp, setActive } = useSignUp();
@@ -62,6 +63,14 @@ const SignUp = () => {
       });
 
       if (completeSignUp.status === 'complete') {
+        await fetchAPI('/(api)/user', {
+          method: 'POST',
+          body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            clerkId: completeSignUp.createdUserId,
+          }),
+        });
         await setActive({ session: completeSignUp.createdSessionId });
         setVerification({
           ...verification,
