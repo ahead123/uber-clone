af9030c5618ea4735a17e7a92734dfcb79d16166
Arthur Head <arthurhead@Arthurs-MacBook-Pro.local>
2024-11-02 15:30:48 -0700

    added nav tabs and signin login

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,20 +1,44 @@
 import { Text, ScrollView, View, Image } from 'react-native';
-import { Link } from 'expo-router';
+import { Link, useRouter } from 'expo-router';
 import { icons, images } from '@/constants';
 import InputField from '@/components/InputField';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import CustomButton from '@/components/CustomButton';
 import OAuth from '@/components/OAuth';
+import { useSignIn } from '@clerk/clerk-expo';
 
 const SignIn = () => {
+  const { signIn, setActive, isLoaded } = useSignIn();
+  const router = useRouter();
+
   const [form, setForm] = useState({
     email: '',
     password: '',
   });
 
-  const onSignInPress = async () => {
-    console.log('form', form);
-  };
+  const onSignInPress = useCallback(async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: form.email,
+        password: form.password,
+      });
+
+      if (signInAttempt.status === 'complete') {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.replace('/');
+      } else {
+        // See https://clerk.com/docs/custom-flows/error-handling
+        // for more info on error handling
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, [isLoaded, form.email, form.password]);
 
   return (
     <ScrollView className="flex-1 bg-white">
